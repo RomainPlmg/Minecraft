6b6c311fd55562b024c5b9730143d44e59165ecc
RomainPlmg <romain.plumaugat@protonmail.com>
2026-05-05 16:12:15 +0200

    Fix timer bug during first update

--- engine/include/opticrafter/Timer.h
+++ engine/include/opticrafter/Timer.h
@@ -13,8 +13,8 @@ class Timer {
     float dt() { return m_dt; }  // Return delta time in second
 
    private:
-    uint64_t m_last_time;  // Time since last call
-    float m_dt;            // Elapsed time between two frames in seconds
+    uint64_t m_last_time = 0;  // Time since last call
+    float m_dt = 0.f;          // Elapsed time between two frames in seconds
 };
 
 }  // namespace opticrafter
--- engine/src/core/Timer.cpp
+++ engine/src/core/Timer.cpp
@@ -7,16 +7,13 @@
 namespace opticrafter {
 
 void Timer::reset() {
-    m_dt = 0.0;
-    m_last_time = 0;
+    m_dt = 0.f;
+    m_last_time = SDL_GetPerformanceCounter();
 }
 
 void Timer::update() {
     uint64_t now = SDL_GetPerformanceCounter();
-
-    if (now - m_last_time != 0) {
-        m_dt = (float)(now - m_last_time) / (float)SDL_GetPerformanceFrequency();
-    }
+    m_dt = (float)(now - m_last_time) / (float)SDL_GetPerformanceFrequency();
     m_last_time = now;
 }
 
