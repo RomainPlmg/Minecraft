1bc6101394460cd60f867aef2fdc7612e0ff5ff3
Romain Plumaugat <romain.plumaugat@microchip.com>
2026-05-19 15:38:45 +0200

    [Engine] Add SSBO & Indirect buffer support

--- engine/include/opticrafter/Buffer.h
+++ engine/include/opticrafter/Buffer.h
@@ -15,6 +15,7 @@ class VertexBuffer {
     VertexBuffer(VertexBuffer&& other) noexcept;
     VertexBuffer& operator=(VertexBuffer&& other) noexcept;
 
+    void updateSubData(size_t offset, std::span<const std::byte> data);
     void bind() const;
     void unbind() const;
     uint32_t id() const { return m_id; }
@@ -42,6 +43,42 @@ class IndexBuffer {
    private:
     uint32_t m_id = 0;
     uint32_t m_count = 0;
+};
+
+class ShaderStorageBuffer {
+   public:
+    ShaderStorageBuffer();
+    ~ShaderStorageBuffer();
+
+    ShaderStorageBuffer(ShaderStorageBuffer&& other) noexcept;
+    ShaderStorageBuffer& operator=(ShaderStorageBuffer&& other) noexcept;
+
+    void setData(std::span<const std::byte> data);
+    void bind(uint32_t binding_point);
+    void unbind(uint32_t binding_point);
+
+    uint32_t id() const { return m_id; }
+
+   private:
+    uint32_t m_id = 0;
+};
+
+class IndirectBuffer {
+   public:
+    IndirectBuffer();
+    ~IndirectBuffer();
+
+    IndirectBuffer(IndirectBuffer&& other) noexcept;
+    IndirectBuffer& operator=(IndirectBuffer&& other) noexcept;
+
+    void setData(std::span<const std::byte> data);
+    void bind() const;
+    void unbind() const;
+
+    uint32_t id() const { return m_id; }
+
+   private:
+    uint32_t m_id = 0;
 };
 
 }  // namespace opticrafter
--- engine/src/renderer/Buffer.cpp
+++ engine/src/renderer/Buffer.cpp
@@ -27,6 +27,10 @@ VertexBuffer& VertexBuffer::operator=(VertexBuffer&& other) noexcept {
     return *this;
 }
 
+void VertexBuffer::updateSubData(size_t offset, std::span<const std::byte> data) {
+    glNamedBufferSubData(m_id, offset, data.size_bytes(), data.data());
+}
+
 void VertexBuffer::bind() const { glBindBuffer(GL_ARRAY_BUFFER, m_id); }
 
 void VertexBuffer::unbind() const { glBindBuffer(GL_ARRAY_BUFFER, 0); }
@@ -58,5 +62,65 @@ IndexBuffer& IndexBuffer::operator=(IndexBuffer&& other) noexcept {
 void IndexBuffer::bind() const { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_id); }
 
 void IndexBuffer::unbind() const { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); }
+
+// ============================================================================
+// Shader Storage Buffer (SSBO)
+// ============================================================================
+ShaderStorageBuffer::ShaderStorageBuffer() { glCreateBuffers(1, &m_id); }
+
+ShaderStorageBuffer::~ShaderStorageBuffer() {
+    if (m_id) glDeleteBuffers(1, &m_id);
+}
+
+ShaderStorageBuffer::ShaderStorageBuffer(ShaderStorageBuffer&& other) noexcept : m_id(other.m_id) { other.m_id = 0; }
+
+ShaderStorageBuffer& ShaderStorageBuffer::operator=(ShaderStorageBuffer&& other) noexcept {
+    if (this != &other) {
+        if (m_id) glDeleteBuffers(1, &m_id);
+        m_id = other.m_id;
+        other.m_id = 0;
+    }
+    return *this;
+}
+
+void ShaderStorageBuffer::setData(std::span<const std::byte> data) {
+    glNamedBufferData(m_id, data.size_bytes(), data.data(), GL_DYNAMIC_DRAW);
+}
+
+void ShaderStorageBuffer::bind(uint32_t binding_point) {
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, binding_point, m_id);
+}
+
+void ShaderStorageBuffer::unbind(uint32_t binding_point) {
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, binding_point, 0);
+}
+
+// ============================================================================
+// Indirect Buffer
+// ============================================================================
+IndirectBuffer::IndirectBuffer() { glCreateBuffers(1, &m_id); }
+
+IndirectBuffer::~IndirectBuffer() {
+    if (m_id) glDeleteBuffers(1, &m_id);
+}
+
+IndirectBuffer::IndirectBuffer(IndirectBuffer&& other) noexcept : m_id(other.m_id) { other.m_id = 0; }
+
+IndirectBuffer& IndirectBuffer::operator=(IndirectBuffer&& other) noexcept {
+    if (this != &other) {
+        if (m_id) glDeleteBuffers(1, &m_id);
+        m_id = other.m_id;
+        other.m_id = 0;
+    }
+    return *this;
+}
+
+void IndirectBuffer::setData(std::span<const std::byte> data) {
+    glNamedBufferData(m_id, data.size_bytes(), data.data(), GL_DYNAMIC_DRAW);
+}
+
+void IndirectBuffer::bind() const { glBindBuffer(GL_DRAW_INDIRECT_BUFFER, m_id); }
+
+void IndirectBuffer::unbind() const { glBindBuffer(GL_DRAW_INDIRECT_BUFFER, 0); }
 
 }  // namespace opticrafter
